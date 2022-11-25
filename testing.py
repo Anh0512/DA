9dad2ff8b420a5b1af30e9a2d34c272c25726fff
Anh0512 <105800460+Anh0512@users.noreply.github.com>
2022-11-25 17:58:14 +0700

    Delete testing.py

--- testing.py
+++
@@ -1,8 +0,0 @@
-from tracemalloc import Snapshot
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt 
-import seaborn as sns
-df = pd.read_excel('house_price_dống-da.xlsx')
-print(df)
-
